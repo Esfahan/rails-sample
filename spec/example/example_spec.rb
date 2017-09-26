1431f9804f19baa3a86075a87e4f8ce40b0825b0
Esfahan <calito0131@gmail.com>
2017-09-26 15:30:20 +0900

    [Add] added rails application

---
+++ spec/example/example_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, :type => 'helper' do
+  describe "example spec" do
+
+    it 'should be 2' do
+      expect(1+1).to eq(2)
+    end
+  end
+end
