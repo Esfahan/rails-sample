c91ed8abc6b5e0a65347e95fc17f093ebd22e772
Esfahan <calito0131@gmail.com>
2017-09-26 21:31:33 +0900

    [Debug] debug ci

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd rails-sample
-bundle install --path vendor/bundle
+bundle install --path build-cache
 bundle exec rspec spec
+
+cd ../
+cp -rp build-cache store-cache
+
+ls build-cache
+ls store-cache
